a0f1d51d159b0733d39bf21f8e5c3672ac34d8de
caplearazvan <82647846+caplearazvan@users.noreply.github.com>
2021-07-01 10:56:01 +0300

    Create Ex5.py

---
+++ # 5. Reverse a given number and return true if it is the same as the original number.  num=int(input("Enter a number:\n")) list1=list() list_reversed=list()  for i  in range(num):     digit=num%10     list_reversed.append(digit)     num = num/Ex5.py
@@ -0,0 +1,31 @@
+
+# 5. Reverse a given number and return true if it is the same as the original number.
+
+num=int(input("Enter a number:\n"))
+list1=list()
+list_reversed=list()
+
+for i  in range(num):
+    digit=num%10
+    list_reversed.append(digit)
+    num = num // 10
+    if num==0:
+        break
+
+decrement=0
+lenght=len(list_reversed)-1
+
+for i in range(len(list_reversed)):
+    list1.append(list_reversed[lenght])
+    lenght-=1
+    decrement+=1
+    if i==4:
+        break
+
+if list1==list_reversed:
+    print("the reverse of the number is the same as the number")
+else:
+    print("the reverse of the number is different as the number")
+
+
+
